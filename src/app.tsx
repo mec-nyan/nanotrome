28e6904abe88b64377fc2430b3a1710d0d55397b
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-06-04 20:36:52 -0300

    Add styles for main components.

--- src/app.tsx
+++ src/app.tsx
@@ -7,14 +7,22 @@ const maxBpm = 300
 const minBpm = 40
 
 export function App() {
-  const [bpm, setBpm] = useState(baseBpm)
 
   return (
     <div className='app'>
       <AppName />
+      <Metronome />
+      <Footer />
+    </div>
+  )
+}
+
+function Metronome() {
+  const [bpm, setBpm] = useState(baseBpm)
+  return (
+    <div className='metronome'>
       <Display bpm={bpm} />
       <Controls bpm={bpm} setBpm={setBpm} />
-      <Footer />
     </div>
   )
 }
@@ -29,9 +37,11 @@ function AppName() {
 
 function Display({bpm}: {bpm: number}) {
   return (
-    <div className='display'>
-      <div className='display-tag'>BPM</div>
-      <div className='bpm'>{bpm}</div>
+    <div className='display-container'>
+      <div className='display'>
+        <div className='display-tag'>BPM</div>
+        <div className='bpm'>{bpm}</div>
+      </div>
     </div>
   )
 }
@@ -55,11 +65,11 @@ function Controls({bpm, setBpm}: {bpm: number, setBpm: (n: number) => void}) {
 
   return (
     <div className='controls'>
-      <button class='control plus-one' onClick={() => handleBpmChange(bpm, 1)}>+1</button>
-      <button class='control minus-one' onClick={() => handleBpmChange(bpm, -1)}>-1</button>
-      <button class='control plus-ten' onClick={() => handleBpmChange(bpm, 10)}>+10</button>
-      <button class='control minus-ten' onClick={() => handleBpmChange(bpm, -10)}>-10</button>
-      <button class='control reset' onClick={() => setBpm(120)}>reset</button>
+      <button class='control plus-one' onClick={() => handleBpmChange(bpm, 1)}>＋1</button>
+      <button class='control minus-one' onClick={() => handleBpmChange(bpm, -1)}>−1</button>
+      <button class='control plus-ten' onClick={() => handleBpmChange(bpm, 10)}>＋10</button>
+      <button class='control minus-ten' onClick={() => handleBpmChange(bpm, -10)}>−10</button>
+      <button class='control reset' onClick={() => setBpm(120)}>Reset</button>
     </div>
   )
 }
@@ -67,7 +77,7 @@ function Controls({bpm, setBpm}: {bpm: number, setBpm: (n: number) => void}) {
 function Footer() {
   return (
     <footer>
-      Made with # by <span className='author'>nano</span>.
+      Made with <span className='material-symbols-outlined love'>favorite</span> by <span className='author'>nano</span>.
     </footer>
   )
 }
