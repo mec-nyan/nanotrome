8cf6a33122533e54cac8054460d1160a60fadd51
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-06-04 19:52:56 -0300

    Add main components: Display, Controls, Footer.

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,20 @@
+import { useState } from 'preact/hooks'
 import './app.css'
 
+// TODO: Check these values.
+const baseBpm = 120
+const maxBpm = 300
+const minBpm = 40
+
 export function App() {
+  const [bpm, setBpm] = useState(baseBpm)
 
   return (
     <div className='app'>
       <AppName />
+      <Display bpm={bpm} />
+      <Controls bpm={bpm} setBpm={setBpm} />
+      <Footer />
     </div>
   )
 }
@@ -14,5 +24,50 @@ function AppName() {
     <div className='top-bar'>
       <span className='app-name'>nanotrome</span>
     </div>
+  )
+}
+
+function Display({bpm}: {bpm: number}) {
+  return (
+    <div className='display'>
+      <div className='display-tag'>BPM</div>
+      <div className='bpm'>{bpm}</div>
+    </div>
+  )
+}
+
+function Controls({bpm, setBpm}: {bpm: number, setBpm: (n: number) => void}) {
+  function handleBpmChange(bpm: number, increment: number) {
+    const newBpm = bpm + increment
+
+    if (newBpm > maxBpm ) {
+      setBpm(maxBpm)
+      return
+    }
+
+    if (newBpm < minBpm) {
+      setBpm(minBpm)
+      return
+    }
+
+    setBpm(newBpm)
+  }
+
+  return (
+    <div className='controls'>
+      <button class='control plus-one' onClick={() => handleBpmChange(bpm, 1)}>+1</button>
+      <button class='control minus-one' onClick={() => handleBpmChange(bpm, -1)}>-1</button>
+      <button class='control plus-ten' onClick={() => handleBpmChange(bpm, 10)}>+10</button>
+      <button class='control minus-ten' onClick={() => handleBpmChange(bpm, -10)}>-10</button>
+      <button class='control reset' onClick={() => setBpm(120)}>reset</button>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      Made with # by <span className='author'>nano</span>.
+    </footer>
   )
 }
