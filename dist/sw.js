541a5960ee61157549d509de372caed7bad5e661
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-06-16 20:23:54 -0300

    PWA: Publish first version (0.1.5).

---
+++ dist/sw.js
@@ -0,0 +1,65 @@
+const VERSION = 'v0.1.5'
+
+const CACHE_NAME = `nanotrome-${VERSION}`
+
+const GPATH = '/nanotrome'
+
+const APP_STATIC_RESOURCES = [
+  `${GPATH}/`,
+  `${GPATH}/index.html`,
+  `${GPATH}/assets/index.css`,
+  `${GPATH}/assets/index.js`,
+  `${GPATH}/manifest.json`,
+  `${GPATH}/icons/icon.svg`,
+  `${GPATH}/screenshots/nanotrome-screenshot-narrow.webp`,
+  `${GPATH}/screenshots/nanotrome-screenshot-wide.webp`,
+]
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME)
+      cache.addAll(APP_STATIC_RESOURCES)
+    })()
+  )
+})
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    (async () => {
+      const names = await caches.keys()
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name)
+          }
+          return undefined
+        })
+      )
+      await clients.claim()
+    })()
+  )
+})
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(caches.match(`${GPATH}/`))
+    return
+  }
+
+  e.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME)
+      const cachedResponse = await cache.match(e.request.url)
+      if (cachedResponse) {
+        return cachedResponse
+      }
+      try {
+        const resp = await fetch(e.request)
+        return resp
+      } catch (err) {
+        return new Response(null, { status: 404 })
+      }
+    })()
+  )
+})
